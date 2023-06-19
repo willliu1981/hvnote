4b6d3643bda0e2a5b2ebd1751fe7e4fd75ecf3e9
willliu1981 <h111107231675>
2023-06-20 00:07:05 +0800

    測試 修改table 欄位結構

--- app/src/main/java/idv/kuan/hvnote/MainActivity.java
+++ app/src/main/java/idv/kuan/hvnote/MainActivity.java
@@ -25,23 +25,25 @@ public class MainActivity extends AppCompatActivity {
         Connection connection = dbFactory.getConnection("android1");
         //System.out.println("xxx MA:" + connection);
 
+        //changeTable(connection);
+
         testSave(connection);
 
-        testQuery(connection,2);
+        testQuery(connection, 11);
 
     }
 
     private void testSave(Connection connection) {
-        String sql = "insert into statement_table (statement,category,is_favorite,is_archived,at_created,at_updated)" +
-                "values (?,?,?,?,?,?)";
+        String sql = "insert into statement_table (statement)" +
+                "values (?)";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, "B3 1號溫停止" + (int) (Math.random() * 10000));
-            preparedStatement.setString(2, "房務");
-            preparedStatement.setInt(3, 0);
-            preparedStatement.setInt(4, 0);
-            preparedStatement.setString(5, "2023-06-19 22:10:02");
-            preparedStatement.setString(5, "2023-06-19 22:10:02");
+            //preparedStatement.setString(2, "房務");
+            //preparedStatement.setInt(3, 0);
+            //preparedStatement.setInt(4, 0);
+            //preparedStatement.setString(5, "2023-06-19 22:10:02");
+            //preparedStatement.setString(5, "2023-06-19 22:10:02");
 
             preparedStatement.execute();
         } catch (SQLException e) {
@@ -50,22 +52,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void testQuery(Connection connection, int id) {
-        String sql = "select * from statement_table where id=?";
+        String sql = "select * from statement_table ";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setInt(1, id);
+            //preparedStatement.setInt(1, id);
 
             ResultSet resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()){
+            int idx = 1;
+            while (resultSet.next()) {
                 String statement = resultSet.getString("statement");
+                String atCreated = resultSet.getString("at_created");
+                String category = resultSet.getString("category");
+                //int isFavorite = resultSet.getInt("is_favorite");
 
 
-                System.out.println("xxx MA :"+statement);
+                System.out.println("xxx MA ------:" + idx++);
+                System.out.println("xxx MA statement:" + statement);
+                System.out.println("xxx MA atCreated:" + atCreated);
+                System.out.println("xxx MA category:" + category);
+
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private void changeTable(Connection connection) {
+        try {
+            String sql0 = "DROP TABLE statement_table_temp";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql0);
+            preparedStatement.execute();
+
+            String sql1 = "CREATE TABLE \"statement_table_temp\" (" +
+                    "\"id\"INTEGER NOT NULL UNIQUE," +
+                    "\"statement\"TEXT NOT NULL," +
+                    "\"category\"TEXT DEFAULT 'COMMON'," +
+                    "\"is_favorite\"INTEGER DEFAULT 0," +
+                    "\"is_archived\"INTEGER DEFAULT 0," +
+                    "\"at_created\"TEXT DEFAULT CURRENT_TIMESTAMP," +
+                    "\"at_updated\"TEXT DEFAULT CURRENT_TIMESTAMP," +
+                    "PRIMARY KEY(\"id\" AUTOINCREMENT)" +
+                    ")";
+
+            preparedStatement.execute(sql1);
+
+            String sql2 = "INSERT INTO statement_table_temp SELECT * FROM statement_table";
+            preparedStatement.execute(sql2);
+
+            String sql3 = "DROP TABLE statement_table";
+            preparedStatement.execute(sql3);
+
+            String sql4 = "ALTER TABLE statement_table_temp RENAME TO statement_table";
+            preparedStatement.execute(sql4);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
