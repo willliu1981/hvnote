78a4da2c7283b1bce4a7cd030f7549c9ab007cc1
willliu1981 <h111107231675>
2023-06-24 00:58:53 +0800

    增加 修改table結構 的工具類

--- app/src/main/java/idv/kuan/hvnote/MainActivity.java
+++ app/src/main/java/idv/kuan/hvnote/MainActivity.java
@@ -9,14 +9,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
 
 import idv.kuan.androidlib.databases.provider.AndroidDBFactory;
 import idv.kuan.libs.databases.BaseDBFactory;
 import idv.kuan.libs.databases.DBFactoryCreator;
+import idv.kuan.libs.databases.utils.TableSchemaModifier;
+import idv.kuan.libs.date.TimestampConverter;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,18 +24,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BaseDBFactory dbFactory = DBFactoryCreator.getFactory(new AndroidDBFactory(this));
-        System.out.println("xxx MA 1:" + dbFactory);
-        dbFactory.config("android1", "hv.db", "hv.db");
-        BaseDBFactory dbFactory2 = DBFactoryCreator.getFactory("android1");
-        System.out.println("xxx MA 2:" + dbFactory2);
 
-        BaseDBFactory dbFactory3 = DBFactoryCreator.getFactory(new AndroidDBFactory(this));
-        System.out.println("xxx MA 3:" + dbFactory3);
-        dbFactory3.config("android2", "hv.db", "hv.db");
-        BaseDBFactory dbFactory4 = DBFactoryCreator.getFactory("android2");
-        System.out.println("xxx MA 4:" + dbFactory4);
-        /*
+        //*
 
         // DBFactory dbFactory = new AndroidDBFactory(this);
 
@@ -47,11 +35,25 @@ public class MainActivity extends AppCompatActivity {
 
         //changeTable(connection);
 
+
+
+
+        String sql = "CREATE TABLE \"statement_table\" (" +
+                "\"id\"INTEGER NOT NULL UNIQUE," +
+                "\"statement\"TEXT NOT NULL," +
+                "\"category\"TEXT DEFAULT 'COMMON'," +
+                "\"is_favorite\"INTEGER DEFAULT 0," +
+                "\"is_archived\"INTEGER DEFAULT 0," +
+                "\"at_created\"TEXT DEFAULT CURRENT_TIMESTAMP," +
+                "\"at_updated\"TEXT DEFAULT CURRENT_TIMESTAMP," +
+                "PRIMARY KEY(\"id\" AUTOINCREMENT)" +
+                ")";
+        TableSchemaModifier.evolveTableStructure(dbFactory.getConnection(), "statement_table",
+                "statement_table", sql);
+
         testSave();
-
         testQueryAll();
-
-         */
+        //*/
 
     }
 
@@ -101,8 +103,8 @@ public class MainActivity extends AppCompatActivity {
 
                 if (atCreated != null) {
                     Timestamp timestamp = Timestamp.valueOf(atCreated);
-                    GMTtoLocal(timestamp);
-
+                    Timestamp timestamp1 = TimestampConverter.GMTtoLocal(timestamp);
+                    System.out.println("xxx MA timestamp1:" + timestamp1);
                 }
 
             }
@@ -112,58 +114,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void changeTable(Connection connection) {
-        try {
-            String sql0 = "DROP TABLE statement_table_temp";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql0);
-            preparedStatement.execute();
 
-            String sql1 = "CREATE TABLE \"statement_table_temp\" (" +
-                    "\"id\"INTEGER NOT NULL UNIQUE," +
-                    "\"statement\"TEXT NOT NULL," +
-                    "\"category\"TEXT DEFAULT 'COMMON'," +
-                    "\"is_favorite\"INTEGER DEFAULT 0," +
-                    "\"is_archived\"INTEGER DEFAULT 0," +
-                    "\"at_created\"TEXT DEFAULT CURRENT_TIMESTAMP," +
-                    "\"at_updated\"TEXT DEFAULT CURRENT_TIMESTAMP," +
-                    "PRIMARY KEY(\"id\" AUTOINCREMENT)" +
-                    ")";
-
-            preparedStatement.execute(sql1);
-
-            String sql2 = "INSERT INTO statement_table_temp SELECT * FROM statement_table";
-            preparedStatement.execute(sql2);
-
-            String sql3 = "DROP TABLE statement_table";
-            preparedStatement.execute(sql3);
-
-            String sql4 = "ALTER TABLE statement_table_temp RENAME TO statement_table";
-            preparedStatement.execute(sql4);
-
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private Timestamp GMTtoLocal(Timestamp gmt) {
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
-        try {
-            final Date parse1 = formatter.parse(gmt.toString());
-
-            formatter.setTimeZone(TimeZone.getTimeZone(TimeZone.getDefault().getID()));
-            final String format2 = formatter.format(parse1);
-            System.out.println("xxx MA format2:" + format2);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-
-        return null;
-    }
 
 
 }
