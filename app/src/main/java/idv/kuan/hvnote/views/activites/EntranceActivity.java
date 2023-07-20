c3f2e5c6e76e246eaf1e9ca2de80be5aaa67aeca
willliu1981 <h111107231675>
2023-07-20 23:45:34 +0800

    取消使用androidgitversion, 測試 table 是否更新

--- app/src/main/java/idv/kuan/hvnote/views/activites/EntranceActivity.java
+++ app/src/main/java/idv/kuan/hvnote/views/activites/EntranceActivity.java
@@ -9,6 +9,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import idv.kuan.hvnote.R;
 import idv.kuan.kuanandroidlibs.databases.provider.AndroidDBFactory;
@@ -55,20 +58,55 @@ public class EntranceActivity extends AppCompatActivity {
         System.out.println("dbg EA: table is exist? " + isTableExist);
 
         //*
-        if (isTableExist != null && !isTableExist) {
-            String sql = "CREATE TABLE \"memo_table\" ( " +
-                    " \"id\" INTEGER NOT NULL UNIQUE, " +
-                    " \"title\" TEXT, " +
-                    " \"category\" TEXT DEFAULT 'COMMON', " +
-                    " \"content\" TEXT, " +
-                    " \"is_important\" INTEGER DEFAULT 0, " +
-                    " \"is_completed\" INTEGER DEFAULT 0, " +
-                    " \"at_created\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
-                    " \"at_updated\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
-                    " PRIMARY KEY(\"id\" AUTOINCREMENT) " +
-                    ")";
+        if (isTableExist != null) {
+            if (!isTableExist) {
 
-            TableSchemaModifier.createNew(conn, sql);
+                String sql = "CREATE TABLE \"memo_table\" ( " +
+                        " \"id\" INTEGER NOT NULL UNIQUE, " +
+                        " \"title\" TEXT, " +
+                        " \"category\" TEXT DEFAULT 'COMMON', " +
+                        " \"content\" TEXT, " +
+                        " \"is_important\" INTEGER DEFAULT 0, " +
+                        " \"is_completed\" INTEGER DEFAULT 0, " +
+                        " \"at_created\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                        " \"at_updated\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                        " PRIMARY KEY(\"id\" AUTOINCREMENT) " +
+                        ")";
+
+                TableSchemaModifier.createNew(conn, sql);
+            } else {
+                String sql = "CREATE TABLE \"memo_table\" ( " +
+                        " \"id\" INTEGER NOT NULL UNIQUE, " +
+                        " \"title\" TEXT, " +
+                        " \"category\" TEXT DEFAULT 'COMMON2', " +
+                        " \"content\" TEXT, " +
+                        " \"is_important\" INTEGER DEFAULT 0, " +
+                        " \"is_completed\" INTEGER DEFAULT 0, " +
+                        " \"at_created\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                        " \"at_updated\" TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                        " PRIMARY KEY(\"id\" AUTOINCREMENT) " +
+                        ")";
+                TableSchemaModifier.evolveTableStructure(conn, "memo_table", "memo_table", sql);
+
+
+            }
+        }
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement("PRAGMA table_info(memo_table)");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String type = resultSet.getString("type");
+                String dflt_value = resultSet.getString("dflt_value");
+
+                System.out.println("xxx EA:name= " + name + " , type=" + type + " , dflt_value=" + dflt_value);
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
 
         // */
